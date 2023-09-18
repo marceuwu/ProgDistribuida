109aa51230cd5e2a315207a109f1dd6424f6970f
marceuwu <106568800+marceuwu@users.noreply.github.com>
2023-09-17 18:48:58 -0600

    actualización

---
+++ PruebaSB-1/src/main/java/progra/dist/PruebaSB/PruebaSb1Application.java
@@ -0,0 +1,13 @@
+package progra.dist.PruebaSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSb1Application.class, args);
+	}
+
+}
---
+++ PruebaSB-1/src/test/java/progra/dist/PruebaSB/PruebaSb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package progra.dist.PruebaSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
